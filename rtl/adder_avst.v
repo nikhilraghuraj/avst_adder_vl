3c20d6be50a02675e5c6494138b289c7581353b4
Puneet Goel <puneet@coverify.com>
2023-11-27 17:35:07 +0530

    Some bug fixes to the design

--- rtl/adder_avst.v
+++ rtl/adder_avst.v
@@ -20,7 +20,7 @@ module adder_avst(clk, reset, data_in, end_in, valid_in, ready_in,
    reg 		end_out;
    reg 		ready_in; 
 
-   reg [1:0] 	count_out;
+   reg [2:0] 	count_out;
    reg [31:0] 	sum;
 
    always @(posedge clk) begin
@@ -29,17 +29,44 @@ module adder_avst(clk, reset, data_in, end_in, valid_in, ready_in,
       end
       else
 	case(count_out)
-	  3: data_out <= sum[31:24];
-	  2: data_out <= sum[23:16];
-	  1: data_out <= sum[15:8];
-	  0: data_out <= sum[7:0];
+	  4: begin
+	     data_out <= sum[31:24];
+	     end_out <= 0;
+	     valid_out <= 1;
+	     ready_in <= 0;
+	     end
+	  3: begin
+	     data_out <= sum[23:16];
+	     end_out <= 0;
+	     valid_out <= 1;
+	     ready_in <= 0;
+	     end
+	  2: begin
+	     data_out <= sum[15:8];
+	     end_out <= 0;
+	     valid_out <= 1;
+	     ready_in <= 0;
+	     end
+	  1: begin
+	     data_out <= sum[7:0];
+	     end_out <= 1;
+	     valid_out <= 1;
+	     ready_in <= 0;
+	     end
+	  0: begin
+	     data_out <= 0;
+	     end_out <= 0;
+	     valid_out <= 0;
+	     ready_in <= 1;
+	     sum <= 0;
+	     end
 	endcase
    end
    
    always @(posedge clk) begin
       if(reset) begin
 	 // reset the signals
-	 count_out <= 0;
+	 count_out <= 'b111;
 	 valid_out <= 0;
 	 end_out <= 0;
 	 ready_in <= 1;
@@ -50,22 +77,22 @@ module adder_avst(clk, reset, data_in, end_in, valid_in, ready_in,
 	    sum <= sum + {24'b0, data_in};
 	    if (end_in) begin
 	       ready_in <= 0;
-	       count_out <= 3;
+	       count_out <= 4;
 	    end
 	 end
 	 if (ready_in == 0) begin
-	    if (count_out == 0) begin
-	       end_out <= 1;
-	    end
-	    else valid_out <= 1;
+	    // if (count_out == 0) begin
+	    //    end_out <= 1;
+	    // end
+	    // else valid_out <= 1;
 	    if (ready_out == 1) begin
 	       count_out <= count_out - 1;
-	       if (end_out == 1) begin
-		  valid_out <= 0;
-		  ready_in <= 1;
-		  end_out <= 0;
-		  sum <= 0;
-	       end
+	       // if (end_out == 1) begin
+	       // 	  valid_out <= 0;
+	       // 	  ready_in <= 1;
+	       // 	  end_out <= 0;
+	       // 	  sum <= 0;
+	       // end
 	    end
 	 end
       end
